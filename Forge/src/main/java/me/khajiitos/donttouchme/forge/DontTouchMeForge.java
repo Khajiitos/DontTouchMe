ca3d2c0b29681f01271e1ad407ce1c4ae877d9bf
khajiitos <khajiit112@gmail.com>
2023-08-27 21:17:27 +0200

    Don't create config on server on Forge

--- Forge/src/main/java/me/khajiitos/donttouchme/forge/DontTouchMeForge.java
+++ Forge/src/main/java/me/khajiitos/donttouchme/forge/DontTouchMeForge.java
@@ -3,17 +3,21 @@ package me.khajiitos.donttouchme.forge;
 import me.khajiitos.donttouchme.common.DontTouchMe;
 import me.khajiitos.donttouchme.common.config.ClothConfigCheck;
 import me.khajiitos.donttouchme.common.config.ClothConfigScreenMaker;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(DontTouchMe.MOD_ID)
 public class DontTouchMeForge {
     public DontTouchMeForge() {
-        DontTouchMe.init();
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+            DontTouchMe.init();
 
-        if (ClothConfigCheck.isInstalled()) {
-            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ClothConfigScreenMaker::create));
-        }
+            if (ClothConfigCheck.isInstalled()) {
+                ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ClothConfigScreenMaker::create));
+            }
+        });
     }
 }
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Client-sided mod which prevents being pushed by other entities.
 
-[![](http://cf.way2muchnoise.eu/full_904991_downloads.svg)](https://www.curseforge.com/minecraft/mc-mods/i-see-what-you-did-there)
-[![](https://cf.way2muchnoise.eu/versions/904991.svg)](https://www.curseforge.com/minecraft/mc-mods/i-see-what-you-did-there)
+[![](http://cf.way2muchnoise.eu/full_906914_downloads.svg)](https://www.curseforge.com/minecraft/mc-mods/dont-touch-me)
+[![](https://cf.way2muchnoise.eu/versions/906914.svg)](https://www.curseforge.com/minecraft/mc-mods/dont-touch-me)
 
-[![~modrinth~](https://raw.githubusercontent.com/intergrav/devins-badges/main/badges/modrinth_64h.png)](https://modrinth.com/mod/i-see-what-you-did-there)
-[![~curseforge~](https://raw.githubusercontent.com/intergrav/devins-badges/main/badges/curseforge_64h.png)](https://www.curseforge.com/minecraft/mc-mods/i-see-what-you-did-there)
+[![~modrinth~](https://raw.githubusercontent.com/intergrav/devins-badges/main/badges/modrinth_64h.png)](https://modrinth.com/mod/dont-touch-me)
+[![~curseforge~](https://raw.githubusercontent.com/intergrav/devins-badges/main/badges/curseforge_64h.png)](https://www.curseforge.com/minecraft/mc-mods/dont-touch-me)
